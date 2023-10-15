22e837cc325380f4831dab181a702f133470e6bb
Vinicius Almeida <termobin@gmail.com>
2023-10-14 21:26:38 -0300

    Finished section 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,37 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { HighlightedDirective } from "./directives/highlighted.directive";
+import { Observable } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { CoursesService } from "./services/courses.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-
-  courses = COURSES;
-
-  constructor() {
-
-  }
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
+    this.courses$ = this.coursesService.loadCourses();
   }
 
-
-
+  save(course: Course): void {
+    this.coursesService
+      .saveCourse(course)
+      .subscribe(() => console.log("Course saved!"));
+  }
 }
